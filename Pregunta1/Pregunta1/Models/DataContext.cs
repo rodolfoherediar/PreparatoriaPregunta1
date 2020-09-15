0c54c20fc956872ba4da978a7d7be85070ce224c
Rodolfo Heredia <a2019115883@estudiantes.upsa.edu.bo>
2020-09-14 22:46:38 -0400

    Se creo DataContext

---
+++ Pregunta1/Pregunta1/Models/DataContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Pregunta1.Models
+{
+    public class DataContext: DbContext
+    {
+        public DataContext(): base ("DefaultConnection")
+        {
+
+        }
+    }
+}
