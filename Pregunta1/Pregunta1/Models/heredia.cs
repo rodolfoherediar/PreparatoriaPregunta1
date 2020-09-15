355ba92be09b8b981c21af360379e07cbe1a7e2e
Rodolfo Heredia <a2019115883@estudiantes.upsa.edu.bo>
2020-09-14 22:43:34 -0400

    Se creo la clase heredia

---
+++ Pregunta1/Pregunta1/Models/heredia.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace Pregunta1.Models
+{
+    public enum PlaceType
+    {
+        Casa=1,
+        Universidad=2,
+        Colegio=3,
+        Trabajo=4,
+        Restaurante=5
+            
+    }
+    public class heredia
+    {
+        [Key]
+        public int herediaID { get; set; }
+
+        [StringLength(30, ErrorMessage = "The field {0} must contein between {2} and {1} characteres", MinimumLength = 2)]
+        [Required(ErrorMessage = "You must enter the field {0}")]
+        [Display(Name ="Nombre completo")]
+        public string Friendofheredia { get; set; }
+
+        [Required]
+        public PlaceType Place { get; set; }
+
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [DataType(DataType.Date)]
+        [Display(Name ="Cumpleaños")]
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
+        public DateTime Birthday { get; set; }
+    }
+}
